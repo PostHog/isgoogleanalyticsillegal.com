321288337b0e06deb59b97fe232862df0dbf231f
Eli Kinsey <eli@ekinsey.dev>
2022-01-18 12:37:00 -0800

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,11 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
   ],
 };
