b8d75a23d9f677db8e52b2dcac0ced8999bb4c07
Eli Kinsey <eli@ekinsey.dev>
2022-01-17 16:04:31 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Is Google Analytics illegal?`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
